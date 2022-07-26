9661d011da8d23c292e29100e0a5717fd84bd7c5
adzheliev <djalan84@gmail.com>
2022-07-26 16:19:26 +0300

    14.8 Практическая работа выполнена

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def divider (n):
+    number_divider = 1
+    while number_divider <= n:
+        number_divider += 1
+        if n % number_divider == 0:
+            break
+    return number_divider
+
+
+n = int(input('Введите число: '))
+
+divider (n)
+divider = divider (n)
+
+print('Наименьший делитель, отличный от единицы: ',divider)
--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def coin(x, y, r):
+    if (x ** 2 + y ** 2) ** 0.5 <= r:
+        print('Монетка где-то рядом')
+    else:
+        print('Монетки в области нет')
+
+
+print('Введите координаты монетки:')
+x = float(input('X: '))
+y = float(input('Y: '))
+r = float(input('Введите радиус: '))
+
+coin(x, y, r)
