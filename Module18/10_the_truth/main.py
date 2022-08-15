ac43b20b3fcf27566ba41a20d8c7bcc1e8df1184
adzheliev <djalan84@gmail.com>
2022-08-15 17:36:45 +0300

    Module 18 round 2

--- Module18/10_the_truth/main.py
+++ Module18/10_the_truth/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+text = 'vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou fy/dpnqm yDpnqmf jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt uufscf uibo otf/ef uzSfbebcjmj vout/dp djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme wfsof qbtt foumz/tjm omfttV mjdjumzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip'
+
+step = -1
+string = ''
+
+for i in text:
+    if i == '.':
+        step -= 1
+    if ord(i) + step > ord("z"):
+        string += chr(ord(i) + step - 26)
+    else:
+        string += chr(ord(i) + step)
+
+print(string)
+textstring = string.split()
+print(textstring)
+
+shift = 3
+for word in textstring:
+    if '.' in word:
+        shift += 1
+    decoded_word = word[-shift::] + word[:-shift]
+    print(decoded_word)
