ba193620aa38a193670b21fdfd06fb58572a6889
adzheliev <djalan84@gmail.com>
2022-09-02 08:09:46 +0300

    module 20 (task 1-4) done

--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+def my_zip(*args):
+    result = [tuple(struct[i] for struct in map(list, args)) for i in range(min(len(element) for element in args))]
+    return result
+
+
+#print(my_zip([1,2,3], ('a','b','c')))
