5b50c9d4da25acaa8d5ca65cb998b1cab95e3146
adzheliev <djalan84@gmail.com>
2022-09-02 22:45:29 +0300

    Module 21 (tasks 1-6 done, 7 halfdone)

--- Module21/06_deep_copy/main.py
+++ Module21/06_deep_copy/main.py
@@ -13,26 +13,24 @@ site = {
         }
     }
 }
-pprint(site)
 
-def phonesale(n, site):
-    copysite = copy.deepcopy(site)
-    count = 0
-    while count < n:
-        phone = input('Введите название продукта для нового сайта: ')
-        for key, value in copysite.items():
-            if 'title' in key:
-                value[key] = 'Куплю/продам ',phone, 'недорого'
-            if 'h2' in key:
-                value[key] = 'У нас самая низкая цена на', phone
-            return copysite
-        else:
-            for key, value in copy.items():
-                if type(value) == dict:
-                    result = phonesale(n, value)
-                    if result is not None:
-                        return result
+def phonesale(copysite, phone):
+    if 'title' in copysite:
+        copysite['title'] = 'Куплю/продам {} недорого'.format(phone)
+    if 'h2' in copysite:
+        copysite['h2'] = 'У нас самая низкая цена на {}'.format(phone)
+        return copysite
+
+    for subdict in copysite.values():
+        if type(subdict) == dict:
+            result = phonesale(subdict, phone)
+            if result:
+                return copysite
+
 
 n = int(input('Сколько сайтов: '))
-
-print(phonesale(n, site))
+copysite = copy.deepcopy(site)
+for i in range(n):
+    phone = input('Введите название продукта для нового сайта: ')
+    print('Сайт для ', phone)
+    pprint(phonesale(copysite, phone))
--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+def sum(*args):
+    if type(args) == tuple:
+        total = 0
+        for i in args:
+            total += i
+        return total
+
+    elif type(args) == list:
+        total = 0
+        for subelement in args:
+            if type(subelement) == int:
+                total = total + subelement
+            elif type(subelement) == list:
+                sum(subelement)
+        return total
+
+print(sum([[1, 2, [3]], [1], 3]))
+print(sum(1, 2, 3, 4, 5))
