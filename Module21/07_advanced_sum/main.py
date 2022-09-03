42c9f7a821828609f56838808285c29808e83978
adzheliev <djalan84@gmail.com>
2022-09-03 16:20:57 +0300

    Module 21 (GMG, it's done!!!!)

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -3,10 +3,12 @@ def sum(*args):
     for element in args:
         if isinstance(element, int):
             total += element
-        else:
-            total += sum(element)
 
-        return total
+        elif type(element) == list:
+            for item in element:
+                total += sum(item)
+    return total
+
 
 print(sum([[1, 2, [3]], [1], 3]))
 print(sum(1, 2, 3, 4, 5))
