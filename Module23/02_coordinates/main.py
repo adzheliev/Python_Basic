e3cdbd52af01310974fea07423f7d94600af3315
adzheliev <djalan84@gmail.com>
2022-09-14 15:24:18 +0300

    Module 23 (task 1-2 done)

--- Module23/02_coordinates/main.py
+++ Module23/02_coordinates/main.py
@@ -14,26 +14,26 @@ def f2(x, y):
 
 
 try:
-    file = open('coordinates.txt', 'r')
-    for line in file:
-        nums_list = line.split()
-        res1 = f(int(nums_list[0]), int(nums_list[1]))
-        try:
-            res2 = f2(int(nums_list[0]), int(nums_list[1]))
+    with open('coordinates.txt', 'r') as file, open('result.txt', 'w') as file2:
+        for line in file:
+            nums_list = line.split()
+            try:
+                res1 = f(int(nums_list[0]), int(nums_list[1]))
+            except Exception:
+                print("Что-то пошло не так с первой функцией")
+            try:
+                res2 = f2(int(nums_list[0]), int(nums_list[1]))
+            except Exception:
+                print("Что-то пошло не так со второй функцией")
             try:
                 number = random.randint(0, 100)
-                file_2 = open('result.txt', 'w')
-                my_list = sorted([res1, res2, number])
-                file_2.write(' '.join(my_list))
+                my_list = sorted([number, res1, res2])
+                file2.write(' '.join(my_list))
             except Exception:
-                print("Что-то пошло не так")
-        except Exception:
-            print("Что-то пошло не так со второй функцией")
-        finally:
-            file.close()
-            file_2.close()
+                print("Что-то пошло не так с записью в файл")
 except Exception:
-    print("Что-то пошло не так с первой функцией")
+    print("Что-то пошло не так с открытием файла")
 
 
-# TODO отредактировать и исправить программу
+
+
