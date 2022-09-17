8fa242d92a7adcc15c29cf7e9c6deaa3c2afaa4f
adzheliev <djalan84@gmail.com>
2022-09-17 14:52:23 +0300

    Module 23 (tasks 1-3 done) 4th task more functions - less result

--- Module23/04_registration/main.py
+++ Module23/04_registration/main.py
@@ -1,26 +1,30 @@
-def checkallfields(splitedline):
-    if len(splitedline) != 3:
+def item_checker(line):
+    try:
+        name, email, age = line.split()
+    except IndexError:
+        pass
+    else:
+        if not namecheck(name):
+            raise NameError('Поле имени содержит НЕ только буквы')
+        if not emailcheck(email):
+            raise SyntaxError('Поле «Имейл» НЕ содержит @ и .(точку)')
+        if not agecheck(age):
+            raise ValueError('Поле «Возраст» НЕ является числом от 10 до 99')
+
+
+def namecheck(name):
+    return name.isalpha()
+
+
+def emailcheck(email):
+    if '.' not in email and '@' not in email:
         return False
     else:
         return True
 
 
-def namecheck(splitedline):
-    if not splitedline[0].isalpha():
-        return False
-    else:
-        return True
-
-
-def emailcheck(splitedline):
-    if '.' not in splitedline[1] and '@' not in splitedline[2]:
-        return False
-    else:
-        return True
-
-
-def agecheck(splitedline):
-    if not splitedline[2].isnumeric() or (int(splitedline[2]) < 10 or int(splitedline[2]) > 99):
+def agecheck(age):
+    if not age.isnumeric() or (int(age) < 10 or int(age) > 99):
         return False
     else:
         return True
@@ -29,36 +33,21 @@ def agecheck(splitedline):
 goodfile = open('registrations_good.log', 'a', encoding='utf-8')
 badfile = open('registrations_bad.log', 'a', encoding='utf-8')
 
-flag1 = flag2 = flag3 = flag4 = False
+
 with open('registrations.txt', 'r', encoding='utf-8') as initial:
     for line in initial:
-        splitedline = line.split()
-        if checkallfields(splitedline) == False:
-            badfile.write(' '.join(splitedline) + ' НЕ присутствуют все три поля: IndexError.')
-            badfile.write('\n')
+        try:
+            string = item_checker(line)
+        except IndexError:
+            badfile.write(line + ' НЕ присутствуют все три поля: IndexError.' + '\n')
+        except NameError:
+            badfile.write(line + ' Поле имени содержит НЕ только буквы: NameError' + '\n')
+        except SyntaxError:
+            badfile.write(line + ' Поле «Имейл» НЕ содержит @ и .(точку): SyntaxError' + '\n')
+        except ValueError:
+            badfile.write(line + ' Поле «Возраст» НЕ является числом от 10 до 99: ValueError' + '\n')
         else:
-            flag1 = True
-            if namecheck(splitedline) == False:
-                badfile.write(' '.join(splitedline) + ' Поле имени содержит НЕ только буквы: NameError.')
-                badfile.write('\n')
-            else:
-                flag2 = True
-
-            if emailcheck(splitedline) == False:
-                badfile.write(' '.join(splitedline) + ' Поле «Имейл» НЕ содержит @ и .(точку): SyntaxError.')
-                badfile.write('\n')
-            else:
-                flag3 = True
-
-            if agecheck(splitedline) == False:
-                badfile.write(' '.join(splitedline) + ' Поле «Возраст» НЕ является числом от 10 до 99: ValueError.')
-                badfile.write('\n')
-            else:
-                flag4 = True
-
-            if flag1 == True and flag2 == True and flag3 == True and flag4 == True:
-                goodfile.write(' '.join(splitedline))
-                goodfile.write('\n')
+            goodfile.write(line + '\n')
 
 goodfile.close()
 badfile.close()
